ed39aeb6475df8bacccd0cf6b4c4f6067b887966
Vinicius Mello <vinicius.mello@i.systems.com.br>
2024-10-14 14:10:47 -0300

    feat: add multidimensional model + pickle

--- house-prices.py
+++ house-prices.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
 
 df = pd.read_csv("casas.csv")
-
-colunas = ["tamanho", "preco"]
-df = df[colunas]
 
 X = df.drop("preco", axis=1)
 y = df.preco
@@ -16,3 +14,5 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 modelo = LinearRegression()
 modelo.fit(X_train, y_train)
+
+pickle.dump(modelo, open("modelo.sav", "wb"))
--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from textblob import TextBlob
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
 
-df = pd.read_csv("casas.csv")
-colunas = ["tamanho", "preco"]
-df = df[colunas]
-
-x = df.drop("preco", axis=1)
-y = df.preco
-
-X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.3, random_state=42
-)
-
-modelo = LinearRegression()
-modelo.fit(X_train, y_train)
+colunas = ["tamanho", "ano", "garagem"]
+modelo = pickle.load(open("modelo.sav", "rb"))
 
 app = Flask(__name__)
 
@@ -33,10 +22,13 @@ def sentimento(frase):
     return f"polaridade: {polaridade}"
 
 
-@app.route("/cotacao/<int:tamanho>")
-def cotacao(tamanho):
-    preco = modelo.predict([[tamanho]])
-    return str(preco)
+@app.route("/cotacao/", methods=["POST"])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+
+    return jsonify(preco=preco[0])
 
 
 app.run(debug=True)
